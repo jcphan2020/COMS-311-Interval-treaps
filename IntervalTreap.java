6997191945197bf64e74087d77889b6d1257a35f
=JCPhan <jcphan@iastate.edu>
2020-03-05 14:34:09 -0600

    Update IntervalTreap.java

--- IntervalTreap.java
+++ IntervalTreap.java
@@ -29,10 +29,12 @@ public class IntervalTreap {
 		return this.height;
 	}
 	
-	//Insert nodes into tree
+	//Insert nodes into tree, based on Algorithms from Binary Search Tree, modified
 	public void intervalInsert(Node z) {
 		Random rand = new Random();
-		int priority = rand.nextInt(10 + this.size);
+		this.size = this.size + 1;
+		z.priority = this.size + rand.nextInt(10);
+		z.imax = z.interv.HIGH;
 		Node y = null;
 		Node x = this.root;
 		while(x != null) {
@@ -54,33 +56,17 @@ public class IntervalTreap {
 		}else{
 			y.right = z;
 		}
-
+		Node s = z.parent;
+		while(z.priority < s.priority){
+			if(s.right == z){
+				leftRotate(s);
+			}else{
+				rightRotate(s);
+			}
+			s = z.parent;
+		}
 	}
 	
-	//Rotate left node
-	private void leftRotate(Node x){
-		Node y = x.right;
-		x.right = y.left;
-		if(y.left != null){
-			y.left.parent = x;
-		}
-		y.parent = x.parent;
-		if(x.parent == null){
-			this.root = y;
-		}else if(x == x.parent.left){
-			x.parent.left = y;
-		}else{
-			x.parent.right = y;
-		}
-		y.left = x;
-		x.parent = y;
-	}
-
-	//Rotate right node
-	private void rightRotate(Node x){
-		Node y = x.left;
-	}
-
 	//Implement needed
 	public void intervalDelete(Node z) {
 		
@@ -111,6 +97,83 @@ public class IntervalTreap {
 		List<Interval> lst = new ArrayList<Interval>();
 		
 		return lst;
+	}
+
+	//Making the right child node the parent node with the current node as left child node
+	//Based on Algorithms from Red Black Tree, modified with imax
+	private void leftRotate(Node x){
+		Node y = x.right;
+		x.right = y.left;
+		if(y.left != null) {
+			y.left.parent = x;
+		}
+		y.parent = x.parent;
+		if(x.parent == null) {
+			this.root = y;
+		}else if(x == x.parent.left) {
+			x.parent.left = y;
+		}else{
+			x.parent.right = y;
+		}
+		y.left = x;
+		x.parent = y;
+		imaxFix(y, x);
+	}
+
+	//Making the left child node the parent node with the current node as right child node
+	//Based on Algorithms from Red Black Tree, modified with imax
+	private void rightRotate(Node y){
+		Node x = y.left;
+		y.left = x.right;
+		if(x.right != null) {
+			x.right.parent = y;
+		}
+		x.parent = y.parent;
+		if(y.parent == null) {
+			this.root = x;
+		}else if(y == y.parent.right) {
+			y.parent.right = x;
+		}else {
+			y.parent.left = x;
+		}
+		x.right = y;
+		y.parent = x;
+		imaxFix(x, y);
+	}
+
+	//Reconfigure the two nodes that were rotated, so both have the accurate imax
+	private imaxFix(Node y, Node x) {
+		if(x.left != null && x.right != null){
+			x.imax = max(x.interv.HIGH, x.left.imax, x.right.imax);
+		}else if(x.right != null){
+			x.imax = max(x.interv.HIGH, x.right.imax);
+		}else if(x.left != null){
+			x.imax = max(x.interv.HIGH, x.left.imax);
+		}else{
+			x.imax = x.interv.HIGH;
+		}
+		if(y.left != null && y.right != null){
+			y.imax = max(y.interv.HIGH, y.left.imax, y.right.imax);
+		}else if(x.right != null){
+			y.imax = max(y.interv.HIGH, y.right.imax);
+		}else if(x.left != null){
+			y.imax = max(y.interv.HIGH, y.left.imax);
+		}else{
+			y.imax = y.interv.HIGH;
+		}
+	}
+
+	//Return the largest value of a or b.
+	private int max(int a, int b) {
+		if(a > b) {
+			return a;
+		}
+		return b;
+	}
+
+	//Return the largest of value a, b, or c.
+	private int max(int a, int b, int c) {
+		return max(max(a, b), c);
 	}
 
 	public static void main(String[] args) {
